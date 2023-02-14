ce440a38b6adba286d5d9757f1f9bd838dad817f
mika-alpha <yuukeikioku@gmail.com>
2023-02-13 20:11:59 -0500

    Create server

---
+++ index.js
@@ -0,0 +1,26 @@
+import http from 'http';
+import fs from 'fs';
+
+const hostname = 'localhost';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    const reqUrl = new URL(req.url, `http://${hostname}:${port}`);
+    fs.readFile(reqUrl.pathname === '/' ? 'index.html' :
+     `${reqUrl.pathname.substring(1)}.html`, (err, data) => {
+        if (err) {
+            res.statusCode = 404;
+            fs.readFile('404.html', (err, data) => {
+                return res.end(data);
+            })
+        } else {
+            res.statusCode = 200;
+            return res.end(data);
+        }
+    })
+})
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
